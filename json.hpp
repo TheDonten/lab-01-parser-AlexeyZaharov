77bdfe0d42a59943a1d8b0d05f00280859dcad55
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2018-09-09 01:05:09 +0300

    Update json.hpp

--- json.hpp
+++ json.hpp
@@ -25,6 +25,7 @@ class Json {
 private:
 	std::map <std::string, std::any> map;
 	std::string json_str;
+
 public:
 	Json(const std::string& s) {
 		json_str = read(s);
@@ -33,14 +34,15 @@ public:
 	bool is_array() const {
 		if (json_str.front() == '[' && json_str.back() == ']')
 			return true;
-		else if ( json_str.front() == '{' && json_str.back() == '}' )
-			return false;
 		else
-			throw std::logic_error("not implemented");
+			return false;
 	}
 
 	bool is_object() const {
-		return !(is_array);
+		if (json_str.front() == '{' && json_str.back() == '}')
+			return true;
+		else
+			return false;
 	}
 
 	// Метод возвращает значение по ключу key, если экземпляр является JSON-объектом.
@@ -63,11 +65,83 @@ public:
 		if (object.is_array()) {
 			
 		}
-		else if (object.is_object()) {
+		else 
+			if (object.is_object()) {
+				std::string key;
+				std::any any;
 
-		}
-		else
-			throw std::logic_error("not implemented");
+				while (object.json_str.find(':')) {
+					int begin = object.json_str.find('{');
+					object.json_str[begin] = ' ';
+					int end = object.json_str.rfind('}');
+					object.json_str[end] = ' ';
+
+					begin = object.json_str.find('"');
+					object.json_str[begin] = ' ';
+					end = object.json_str.find('"');
+					object.json_str[end] = ' ';
+					key = object.json_str.substr(begin + 1, end - begin);
+
+					begin = object.json_str.find(':');
+					object.json_str[begin++] = ' ';
+
+					if (object.json_str[begin] == '"') {
+						object.json_str[begin] = ' ';
+						end = object.json_str.find('"');
+						object.json_str[end] = ' ';
+						any = object.json_str.substr(begin + 1, end - begin);
+					}
+					else if (object.json_str[begin] == '{') {
+						int i = 1;
+
+						for (auto ch = begin + 1; i != 0 && ch < object.json_str.length(); ch++) {
+							if (ch == '{')
+								i++;
+							else if (ch == '}')
+								i--;
+							if (i == 0)
+								end = ch;
+						}
+						std::string str = object.json_str.substr(begin + 1, end - begin);
+						any = Json::parse(str);
+					}
+					else if (object.json_str[begin] == '[') {
+						int i = 1;
+
+						for (auto ch = begin + 1; i != 0 && ch < object.json_str.length(); ch++) {
+							if (ch == '[')
+								i++;
+							else if (ch == ']')
+								i--;
+							if (i == 0)
+								end = ch;
+						}
+						std::string str = object.json_str.substr(begin + 1, end - begin);
+						any = Json::parse(str);
+					}
+					else {
+						end = object.json_str.find(',');
+						object.json_str[end] = ' ';
+						std::string str = object.json_str.substr(begin, end - begin);
+						if (str == "false" || str == "true")
+							if (str == "false")
+								any = false;
+							else
+								any = true;
+						else {
+							int number;
+							std::istringstream stream(str);
+							stream >> number;
+							any = number;
+						}
+					}
+				}
+
+				object.map.insert(std::pair<std::string, std::any>(key, any));
+			}
+
+			else
+				throw std::logic_error("not implemented");
 
 		return object;
 	}
