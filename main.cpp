02006222cd17e17e7015f7941861b8025419b0f5
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2018-09-17 02:35:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 #include <sstream>
 #include <nlohmann/json.hpp>
 
-
 using nlohmann::json;
 
 std::string read(std::string string = "") {
 	std::string buf;
-
+	
 	if (string == "") {
 		do {
 			std::getline(std::cin, buf);
@@ -19,30 +18,27 @@ std::string read(std::string string = "") {
 	while (stream >> buf) {
 		string += buf;
 	}
-
 	return string;
 }
 
 int main() {
-	 json data = json::parse(read());
-
-	 json array = json::array();
-
-	 for (auto i : data) {
-		 array.push_back(i);
-	 }
-
-	 data.clear();
-
-	 unsigned int i = 0;
-	 for (auto j : array) {
-		 json object = {
-							{"ticker", array[0][i]},
-							{"id", array[1][i]},
-							{"description", array[2][i++]}
-					   };
-		 data.push_back(object);
-	 }
-   
-   return 0;
+	json data = json::parse(read());
+	json array = json::array();
+	
+	for (auto i : data) {
+		array.push_back(i);
+	}
+	
+	data.clear();
+	unsigned int i = 0;
+	for (auto j : array) {
+		json object = {
+			{"ticker", array[0][i]},
+			{"id", array[1][i]},
+			{"description", array[2][i++]}
+		};
+		data.push_back(object);
+	}
+	
+	return 0;
 }
